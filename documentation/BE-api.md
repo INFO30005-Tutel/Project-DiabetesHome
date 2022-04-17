4401dc6c4063e0bb59fbd5733ff3c922862a465e
haiha1199 <93174686+haiihaa@users.noreply.github.com>
2022-04-17 21:36:52 +0700

    Init user schema

---
+++ documentation/BE-api.md
@@ -0,0 +1,20 @@
+**BACK-END DOCUMENTATIONS**
+
+## Table of contents
+
+- [Router APIs](#router-apis)
+- [Database's structure](#databasesstructure)
+- [Notes](#notes)
+- [Others](#others)
+
+## Router APIs
+
+## Database's structure
+
+<p align="center">
+  <img src="img and resources/DB structure.png"  width="650" >
+</p>
+	
+## Notes
+
+## Others
---
+++ models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+    },
+    clinicId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    clinicianId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false, // Null if clinician
+    },
+    requiredFields: [{
+      type: Number
+    }],
+    // avatar: {
+    //   type: String,
+    //   required: false,
+    // },
+  }
+);
+
+// This code block is for password encryption
+// UserSchema.methods.hashPassword = function (password) {
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+// };
+
+// UserSchema.methods.verifyPassword = function (password) {
+//   return bcrypt.compareSync(password, this.password);
+// };
+
+module.exports = mongoose.model('User', UserSchema);
