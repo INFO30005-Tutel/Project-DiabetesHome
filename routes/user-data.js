c065c18a7631af1e4b7a0d74c5e2a20d5fd44718
NKLam <khoalamvn@gmail.com>
2022-04-26 20:47:39 +1000

    Remove passport authentication for delivery 2

--- routes/user-data.js
+++ routes/user-data.js
@@ -6,7 +6,7 @@ const passport = require('../passport');
 // Other routes from this point require authentication ===========================================
 app.get(
   '/todaydata',
-  passport.authenticate('jwt', { session: false }),
+  //passport.authenticate('jwt', { session: false }),
   controller.getTodayData
 );
 
@@ -28,7 +28,7 @@ app.post(
 // }
 app.put(
   '/userdata',
-  passport.authenticate('jwt', { session: false }), 
+  //passport.authenticate('jwt', { session: false }),
   controller.update
 );
 
@@ -36,6 +36,4 @@ app.put(
 app.get('/userdata', controller.findAll);
 app.get('/userdata/:id', controller.findOne);
 
-
 module.exports = app;
-
