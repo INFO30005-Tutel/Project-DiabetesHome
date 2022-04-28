d54862c9e73aac60a3bd56bd41d049097a4ff756
NKLam <khoalamvn@gmail.com>
2022-04-28 11:46:09 +1000

    Update delivery2-mock.js

--- routes/delivery2-mock.js
+++ routes/delivery2-mock.js
@@ -6,6 +6,13 @@ app.post('/login', controller.mockLogin);
 
 app.post('/sign-up', controller.mockLogin);
 
+// Use to test in POSTMAN
+// Format:
+// {
+//   "type": "0",
+//   "data": "67",
+//   "note": "I eat a lot of sugar"
+// }
 app.put('/userdata', controller.mockUpdate);
 
 module.exports = app;
