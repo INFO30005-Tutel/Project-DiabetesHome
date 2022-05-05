0065e9438971eaa99db3e9125589eb784f11d11f
NKLam <khoalamvn@gmail.com>
2022-05-05 19:39:42 +1000

    Fix relative path for images and links in hbs layout

--- routes/clinician.js
+++ routes/clinician.js
@@ -5,4 +5,8 @@ const controller = require('../controllers/clinician');
 
 app.get('/clinician/:clinician_id', controller.getDashboardData);
 
+// Next 2 route are for testing
+app.get('/clinician/:clinician_id/:id2', controller.getDashboardData);
+app.get('/clinician/:clinician_id/:id2/:id3', controller.getDashboardData);
+
 module.exports = app;
