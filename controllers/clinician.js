60458fc6e88641ddf9351f30ef696674f1d01e3c
haiha1199 <93174686+haiihaa@users.noreply.github.com>
2022-05-12 17:24:03 +0700

    Implement diagram using loop

--- controllers/clinician.js
+++ controllers/clinician.js
@@ -43,10 +43,10 @@ const getPatientData = async (req, res) => {
     recordingTime: recordingTime,
     patientDob: patientDob,
     thresholds: [
-      { thresName: 'Blood glucose level', thresFrom: 110, thresTo: 130, thresUnit: 'nmol/L', thresTime: 'Morning' },
-      { thresName: 'Weight entry', thresFrom: 70, thresTo: 90, thresUnit: 'kg', thresTime: 'Morning' },
-      { thresName: 'Dose of insulin taken per day', thresFrom: 2, thresTo: 3, thresUnit: 'doses', thresTime: 'Afternoon' },
-      { thresName: 'Step count recommended', thresFrom: 4000, thresTo: 6000, thresUnit: 'steps', thresTime: 'Evening' },
+      { thresName: 'Blood glucose level', thresFrom: 110, thresTo: 130, thresUnit: 'nmol/L' },
+      { thresName: 'Weight entry', thresFrom: 70, thresTo: 90, thresUnit: 'kg' },
+      { thresName: 'Dose of insulin taken per day', thresFrom: 2, thresTo: 3, thresUnit: 'doses' },
+      { thresName: 'Step count recommended', thresFrom: 4000, thresTo: 6000, thresUnit: 'steps' },
     ],
     detailedData: [
       { id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }],
